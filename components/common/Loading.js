c1c1e109df6fe79590f8005511a332e6b87b61fe
MinhNN <nnminh742@gmail.com>
2021-11-04 08:07:41 +0700

    Create new Confirm Dialog and Loading

---
+++ components/common/Loading.js
@@ -0,0 +1,27 @@
+
+
+function Loading() {
+    return (
+        <>
+            <h1 className="title-loading">Cooking in progress..</h1>
+            <div id="cooking">
+                <div className="bubble"></div>
+                <div className="bubble"></div>
+                <div className="bubble"></div>
+                <div className="bubble"></div>
+                <div className="bubble"></div>
+                <div id="area">
+                    <div id="sides">
+                        <div id="pan"></div>
+                        <div id="handle"></div>
+                    </div>
+                    <div id="pancake">
+                        <div id="pastry"></div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Loading
